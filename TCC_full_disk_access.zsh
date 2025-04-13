f2bffbf550eb36e0e07768d07403bb225f79fa12
bgkf <108151241+bgkf@users.noreply.github.com>
2025-04-13 13:29:48 -0700

    Update and rename TCC full disk access.zsh to TCC_full_disk_access.zsh

--- TCC_full_disk_access.zsh
+++ TCC_full_disk_access.zsh
@@ -1,5 +1,7 @@
 #! /bin/zsh
 
+# Get all services with full disk access.
+
 sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db << 'END_SQL' 
 .timeout 2000 
 SELECT service, client, client_type, auth_value, auth_reason, auth_version FROM access WHERE service = "kTCCServiceSystemPolicyAllFiles" AND auth_value = 2; 
