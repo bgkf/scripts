2a08ca498cc032933d02b83dd10cd88a6bdf9744
bgkf <108151241+bgkf@users.noreply.github.com>
2025-04-13 13:28:47 -0700

    Add files via upload

---
+++ TCC full disk access.zsh
@@ -0,0 +1,6 @@
+#! /bin/zsh
+
+sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db << 'END_SQL' 
+.timeout 2000 
+SELECT service, client, client_type, auth_value, auth_reason, auth_version FROM access WHERE service = "kTCCServiceSystemPolicyAllFiles" AND auth_value = 2; 
+END_SQL
