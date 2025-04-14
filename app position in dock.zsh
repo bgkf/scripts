6531a4c4cfa097c788c4d6e5d866366e6a4bfaf0
bgkf <108151241+bgkf@users.noreply.github.com>
2025-04-14 07:37:40 -0700

    Add files via upload

---
+++ app position in dock.zsh
@@ -0,0 +1,41 @@
+#! /bin/zsh
+
+# variables
+dockutil=/usr/local/bin/dockutil
+loggedInUser=$(stat -f%Su /dev/console)
+uid=$(id -u "$loggedInUser")
+plist="/Users/$loggedInUser/Library/Preferences/com.apple.dock.plist"
+
+# functions
+runAsUser() {  
+  if [ "$loggedInUser" != "loginwindow" ]; then
+    launchctl asuser "$uid" sudo -u "$loggedInUser" "$@"
+  else
+    echo "No user logged in."
+    exitCode=1
+    exit $exitCode
+  fi
+}
+
+# Check if the the app is in the dock. Create temp file if it is.
+appNameDock="<APPNAME>"
+appInDock=$(runAsUser "${dockutil}" --find ${appNameDock} ${plist} | cut -d ' ' -f 1-3)
+appSection=$(runAsUser "${dockutil}" --find ${appNameDock} ${plist} | cut -d ' ' -f 5)
+appPosition=$(runAsUser "${dockutil}" --find ${appNameDock} ${plist} | cut -d ' ' -f 8)
+
+echo "appNameDock = $appNameDock"
+echo "appInDock = $appInDock"
+echo "appSection = $appSection"
+echo "appPosition = $appPosition"
+
+if [ "$appInDock" = "$appNameDock was found" ]; then
+	touch /opt/BrinkAgentDock.txt
+    echo "$appInDock in the dock in $appSection at position $appPosition."
+    echo "$appInDock;$appSection;$appPosition" >> /opt/BrinkAgentDock.txt
+    runAsUser "${dockutil}" --remove /Applications/$appName
+    echo "$appName was removed from the dock"
+else
+	echo "$appName is NOT in the dock."
+fi
+
+exit 0
